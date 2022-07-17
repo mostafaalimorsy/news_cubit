e9d89ec90c0b5c3b091045cd0ee60ef412f92c02
mmorsy <mostafamorsy501@gmail.com>
2022-07-17 05:53:52 +0200

    initial commit

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/mmorsy/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/mmorsy/StudioProjects/news_cubit" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/mmorsy/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/mmorsy/StudioProjects/news_cubit"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/mmorsy/StudioProjects/news_cubit/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/mmorsy/StudioProjects/news_cubit/lib/main.dart"
+)
